1429603e1590e628553578dcdc65d7dd616277be
Youssef Selkani <ynakles@gmail.com>
2019-12-12 00:12:00 +0100

    init

---
+++ Readme.md
@@ -0,0 +1,34 @@
+
+# Monosfer
+
+Flip any React apps to dark mode, which can be easier on the eyes, better for night-time browsing and in some cases, helpful with battery life.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+```
+npm install && npm start
+```
+
+Go to http://localhost:3000
+
+## Deployment
+
+Deploy this on a live system
+
+```
+npm run build
+```
+
+## Author
+
+* **Youssef Selkani** - [Murkstom](https://murkstom.com/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ src/App.js
@@ -0,0 +1,76 @@
+// Youssef Selkani
+// Protobulb.io
+// 2018
+
+import React, { Component } from 'react'
+import { Button, Alert, Jumbotron } from 'reactstrap'
+import './App.css'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      class: '',
+      modal: false
+    }
+  }
+
+  componentDidMount = () => {
+    var mode = localStorage.getItem('mode')
+    if (mode) {
+      this.setState({ class: mode })
+    } else {
+      this.setState({ class: 'default' })
+    }
+  }
+
+  switch = () => {
+    var mode = localStorage.getItem('mode')
+    if (mode === 'lightMode') {
+      localStorage.setItem('mode', 'nightMode')
+      this.setState({ class: 'nightMode' })
+    } else {
+      localStorage.setItem('mode', 'lightMode')
+      this.setState({ class: 'lightMode' })
+    }
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <div className={this.state.class}>
+          <h1>LocalStorage Dark Mode</h1>
+          <h6>Reload this page!</h6>
+          <hr />[ this.state.{this.state.class} ]
+          <Button className='mt-5 mb-5 pl-5 pr-5' onClick={this.switch} color='primary' active>
+            🌗 Switch
+          </Button>
+          <Button href='https://github.com/Protobulb/nightMode' color='link'>
+            Source
+          </Button>
+          <div style={{ maxWidth: '80vh', textAlign: 'justify', margin: '5vh' }}>
+            <h6>
+              Flip any React apps to dark mode, which can be easier on the eyes, better for
+              night-time browsing and in some cases, helpful with battery life.
+            </h6>
+            <hr />
+
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+          </div>
+          <Alert color='light'>
+            <h6>
+              Build a native mobile app in a fraction of the time and cost at{' '}
+              <a className='alert-link' href='https://murkstom.com/?utm_source=darkmode'>
+                murkstom.com
+              </a>
+            </h6>
+          </Alert>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
